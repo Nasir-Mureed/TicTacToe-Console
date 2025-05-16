0e306f3f37716b5b1c0a15befd6256441b476905
User <User@DESKTOP-V1CUUPH>
2025-05-16 05:00:24 -0700

    Add project files.

---
+++ README.md
@@ -0,0 +1,23 @@
+# Tic Tac Toe - Console Game (C++)
+
+This is a simple two-player Tic Tac Toe game built using C++ programming fundamentals. The game runs in the console and uses basic concepts like arrays, functions, loops, and conditionals.
+
+## ?? Features
+- Console-based 3x3 grid game
+- Turn-based two-player mode
+- Win, draw, and Lose
+- Clear and simple UI in text format
+
+## ?? Concepts Used
+- Arrays
+- Functions
+- If-else logic
+- Loops
+- Basic C++ console I/O
+
+## ?? How to Run
+1. Open the project in Visual Studio.
+2. Build and run the project.
+3. Follow on-screen instructions to play.
+
+## ?? Preview
---
+++ game.cpp
@@ -0,0 +1,86 @@
+//#include<iostream>
+//using namespace std;
+//void displayArray(char(*arr)[3], int row, int rowSize);
+//void playGame(char (*arr)[3], int row, int rowSize);
+//void displayWinner(char (*arr)[3], int row, int rowSize);
+//bool checkIfAnyPlayerWin(char(*arr)[3], int row, int rowSize);
+//void inputPlayer1Choice(char(*arr)[3]);
+//void inputPlayer2Choice(char(*arr)[3]);
+//int main()
+//{
+//	const int  row = 3, rowSize = 3;
+//	char arr[row][rowSize] = { '1','2','3','4','5','6','7','8','9' };
+//	displayArray(arr, row, rowSize);
+//	playGame(arr, row, rowSize);
+//}
+//void playGame(char (*arr)[3], int row, int rowSize)
+//{
+//	int choice = 1;
+//	while ((checkIfAnyPlayerWin(arr, row, rowSize) == false) && (choice <= 9))
+//	{
+//		if (choice % 2 == 1)
+//		{
+//			inputPlayer1Choice(arr);
+//		}
+//		else
+//		{
+//			inputPlayer2Choice(arr);
+//		}
+//		displayArray(arr, row, rowSize);
+//		if (checkIfAnyPlayerWin(arr, row, rowSize))
+//		{
+//			displayWinner(arr, row, rowSize);
+//		}
+//		choice = choice + 1;
+//	}
+//	if (choice == 10)
+//	{
+//		cout << "Match has been Tie ! ";
+//	}
+//}
+//void displayArray(char(*arr)[3], int row, int rowSize)
+//{
+//	for (int i = 0;i < row;i = i + 1)
+//	{
+//		for (int j = 0;j < rowSize;j = j + 1)
+//		{
+//			cout << arr[i][j] << "  ";
+//		}
+//		cout << "\n";
+//	}
+//}
+//void inputPlayer1Choice(char(*arr)[3])
+//{
+//	int choice;
+//	cout << "Player 1 : Enter your choice_";
+//	cin >> choice;
+//	int row = (choice - 1) / 3, column= (choice - 1)%3;
+//	arr[row][column] = 'x';
+//}
+//void inputPlayer2Choice(char(*arr)[3])
+//{
+//	int choice;
+//	cout << "Player 2 : Enter your choice_";
+//	cin >> choice;
+//	int row = (choice - 1) / 3, column = (choice - 1) % 3;
+//	arr[row][column] = 'y';
+//}
+//bool checkIfAnyPlayerWin(char(*arr)[3], int row, int rowSize)
+//{
+//	if (((arr[0][0] == arr[0][1]) && (arr[0][0] == arr[0][2])) || ((arr[1][0] == arr[1][1]) && (arr[1][0] == arr[1][2])) || ((arr[2][0] == arr[2][1]) && (arr[2][0] == arr[2][2])) || ((arr[0][0] == arr[1][0]) && (arr[0][0] == arr[2][0])) || ((arr[0][1] == arr[1][1]) && (arr[0][1] == arr[2][1])) || ((arr[0][2] == arr[1][2]) && (arr[0][2] == arr[2][2])) || ((arr[0][0] == arr[1][1]) && (arr[0][0] == arr[2][2])) || ((arr[0][2] == arr[1][1]) && (arr[0][2] == arr[2][0])))
+//	{
+//		return true;
+//	}
+//	return false;
+//}
+//void displayWinner(char (*arr)[3], int row, int rowSize)
+//{
+//	if (((arr[0][0] == arr[0][1]) && (arr[0][0] == arr[0][2]) && (arr[0][0] == 'X')) || ((arr[1][0] == arr[1][1]) && (arr[1][0] == arr[1][2]) && (arr[1][0] == 'X')) || ((arr[2][0] == arr[2][1]) && (arr[2][0] == arr[2][2]) && (arr[2][0] == 'X')) || ((arr[0][0] == arr[1][0]) && (arr[0][0] == arr[2][0]) && (arr[0][0] == 'X')) || ((arr[0][1] == arr[1][1]) && (arr[0][1] == arr[2][1]) && (arr[0][1] == 'X')) || ((arr[0][2] == arr[1][2]) && (arr[0][2] == arr[2][2]) && (arr[0][2] == 'X')) || ((arr[0][0] == arr[1][1]) && (arr[0][0] == arr[2][2]) && (arr[0][0] == 'X')) || ((arr[0][2] == arr[1][1]) && (arr[0][2] == arr[2][0]) && (arr[0][2] == 'X')))
+//	{
+//		cout << "Congratulations ! Player #2 won the Match";
+//	}
+//	else
+//	{
+//		cout << "Congratulations ! Player #1 won the Match";
+//	}
+//}
